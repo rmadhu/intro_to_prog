851b6276cdb73d16b6076e094f1fc08c2c7aed8a
Madhu Ramalingam <ramalingam.madhu@gmail.com>
2015-02-17 16:43:15 +1100

    exercises from the chapter - variables

---
+++ age.rb
@@ -0,0 +1,10 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+#ten = age+10
+#twenty = age+20
+#thirty = age+30
+#forty = age+40
+puts "You will be #{age+10} years old in 10 years"
+puts "You will be #{age+20} years old in 20 years"
+puts "You will be #{age+30} years old in 30 years"
+puts "You will be #{age+40} years old in 40 years"
---
+++ name.rb
@@ -0,0 +1,9 @@
+puts "What's your first name?"
+fname = gets.chomp
+puts "Hello #{fname}"
+
+puts "What's your last name?"
+lname = gets.chomp
+puts fname+ " "+lname
+
+10.times { puts fname }
